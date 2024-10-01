2ee326decaf9b20a50d12e6d0b0a6673d0b2fdbb
Mario Sampietro <mariosam2005@gmail.com>
2024-10-01 12:00:04 +0200

    b

--- app/src/main/java/com/svalero/themoviedb_001/movies_api/MoviesAPI.java
+++ app/src/main/java/com/svalero/themoviedb_001/movies_api/MoviesAPI.java
@@ -5,12 +5,17 @@ import com.svalero.themoviedb_001.json_mapper.MovieResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface MoviesAPI {
     // Routers!!! express.js
-    @GET("movie/popular?api_key=a4baafc5fd87ddc79d6c8548a8559b9d")
-    Call<MovieResponse> getPopularMovies();
+    //@GET("movie/popular?api_key=a4baafc5fd87ddc79d6c8548a8559b9d")
+    //Call<MovieResponse> getPopularMovies();
+    @GET("movie/popular")
+    Call<MovieResponse> getPopularMovies(
+            @Query("api_key") String apiKey
+    );
 
-    @GET("search/movie?a4baafc5fd87ddc79d6c8548a8559b9d")
+    @GET("search/movie")
     Call<MovieResponse> searchMovies(String apiKey);
 }
